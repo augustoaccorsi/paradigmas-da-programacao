3883e3cc51918f63deefe7d0fa15583a3c493a02
Augusto Accorsi <augusto.acorsi@gmail.com>
2018-04-27 20:18:48 -0300

    add exemple files just to make sure that python is working fine

---
+++ desafio1-python/exemplo1.py
@@ -0,0 +1,10 @@
+x = int(input("Informe um número"))
+if x < 0:
+    x = 0
+    print("Número negativo alterado pra zero")
+elif x==0:
+    print("zero")
+elif x==1:
+    print("um")
+else:
+    print("outro")
